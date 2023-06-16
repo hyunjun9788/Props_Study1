3eca2e7c0bd0f64a310511511ccdfad2af696d02
hyunjun9788 <namhyunjun9788@gmail.com>
2023-06-16 15:25:25 +0900

    feat: Card.js/css 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './App.css';
+import Card from "./components/Card";
 
 const App = () => {
   return (
       <>
-        <div className="card profile-container">
+        <Card className="card profile-container">
           <div>이름 : 박재성</div>
           <div>나이 : 20</div>
-        </div>
+        </Card>
 
-        <div className="card product-container">
+        <Card className="card product-container">
           <h3>상품명 : 아이폰</h3>
           <p>가격 : 90만원</p>
-        </div>
+        </Card>
       </>
   );
 }
